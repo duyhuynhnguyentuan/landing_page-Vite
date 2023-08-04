55923bd09bf066bac6f90c51c615e0aa631b1fd4
DUY HUYNH <124075895+duyhuynhnguyentuan@users.noreply.github.com>
2023-08-04 23:55:00 +0700

    first launch

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen px-12 relative overflow-hidden'>
+      {/* navbar */}
+      <div className='absolute -top-12 -left-12 w-[300px] h-[300px] '/>
+  <p className='text-yellow-600 pt-3 text-4xl font-bold '> Tiếng Việt test</p>
+    </div>
+  )
+}
+
+export default App
